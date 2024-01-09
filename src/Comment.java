228159ac08116f8cc90a70c76836e661dde71ce6
dreamik <dream-ik89@naver.com>
2024-01-09 15:29:49 +0900

    단순 줄 수정

--- src/Comment.java
+++ src/Comment.java
@@ -2,10 +2,10 @@ public class Comment {
     public static void main(String[] args){
         System.out.println("hello java 1"); //hello java 1을 출력
 //        System.out.println("이건 안쓸건데 남겨둘래요");
-        /*
+    /*
         System.out.println("여");
         System.out.println("러");
         System.out.println("개");
-        */
+    */
     }
 }
