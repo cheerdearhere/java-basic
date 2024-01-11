4491219154c44238f735b9d1a04d2bbb07d9e625
dreamik <dream-ik89@naver.com>
2024-01-11 19:04:08 +0900

    variable 선언, 초기화, 값 변경

--- src/chapter01_Variable/Var1.java
+++ src/chapter01_Variable/Var1.java
@@ -16,5 +16,23 @@ public class Var1 {
         System.out.println(num1);
         num1+=1;
         System.out.println(num1);
+
+//        변수의 값 변경
+        System.out.println("##변수를 변경하는 경우");
+        int a;  //변수 선언 a(null)
+        a = 10; //변수 초기화 a(10)
+        System.out.println(a);//10
+        a = 50; // 값 변경 a(50)
+        System.out.println(a);//50
+
+//        변수 선언과 초기화 방법
+        System.out.println("##변수를 선언하고 변경하는 여러방법");
+        int b;
+        b=1;
+        System.out.printf("     1. 변수 선언과 초기화를 따로: "+b);
+        int c = 1;
+        System.out.println("    2. 변수 선언과 초기화를 함께: "+c);
+        int d =2, e = 3;
+        System.out.println("    3. 여러개 변수의 선언과 초기화를 한번에: "+d+'/'+e);
     }
 }
---
+++ src/chapter01_Variable/Var2_type.java
@@ -0,0 +1,21 @@
+package chapter01_Variable;
+
+public class Var2_type {
+    public static void main(String[] args){
+//        기본단위 8bit
+        byte    b       = 125;            // -128 ~ 127
+//        정수
+        short   sh      = 32000;          // 2bytes : -32,768 ~ 32,767
+        int     i       = 2100000000;     // 4bytes : -2,147,483,648 ~ 2,147,483,647
+        long    l       = 1000000000200L; // 8bytes : -9,223,372,036,854,775,808 ~ 9,223,372,036,854,775,807
+//        실수: 부동소수점 사용(부호용 1자리+부동소수점)
+        float   f       = 100.0F;         // 4bytes : 7자리 정밀도
+        double  d       = 10.5;           // 8bytes : 15자리 정밀도
+//        문자관련
+        char    c       = 'A';            // 1byte  : 문자 하나
+        String  str     = "String data";  //비정형(final로 선언된 문자 배열)
+//        기타
+        boolean bool    = true;           // 1byte  : true, false
+        var     v       = 1;              // 비정형
+    }
+}
