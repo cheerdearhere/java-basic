c8ab32675626982b013901c3296c208758527823
dreamik <dream-ik89@naver.com>
2024-01-11 19:04:39 +0900

    variable 연습문제

---
+++ src/chapter01_Variable/ex/VarEx1Question.java
@@ -0,0 +1,18 @@
+package chapter01_Variable.ex;
+
+public class VarEx1Question {
+    public static void main(String[] args){
+        /*
+        문제1: 다음 코드에 반복해서 나오는 숫자 4, 3을 다른 숫자로 한번에 변경할 수 있도록 변경하세요.
+           System.out.println(4+3);
+           System.out.println(4-3);
+           System.out.println(4*3);
+         */
+        int num1 = 4;
+        int num2 = 3;
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+    }
+}
+
---
+++ src/chapter01_Variable/ex/VarEx3Question.java
@@ -0,0 +1,16 @@
+package chapter01_Variable.ex;
+
+public class VarEx3Question {
+    public static void main(String[] args){
+        /*
+        문제 3: long 타입의 변수를 선언하고 그 변수를 100억으로 초기화한 후 출력하는 프로그램을 작성하세요.
+        boolean 타입의 변수를 선언하고 그 변수를 true로 초기화한 후 출력하는 프로그램을 작성하세요.
+         */
+        long longNum;
+        longNum= 10000000000L;
+        System.out.println("longNum = " + longNum);
+        boolean booleanVar;
+        booleanVar = true;
+        System.out.println("booleanVar = " + booleanVar);
+    }
+}
