8cd8be62ed9b514ebccf6c87bbfe18c9de82fbf0
dreamik <dream-ik89@naver.com>
2024-01-16 19:40:57 +0900

    문제 수정

--- src/chapter04_loop/ex/Loop4Question.java
+++ src/chapter04_loop/ex/Loop4Question.java
@@ -7,6 +7,13 @@ public class Loop4Question {
     문제4. 피라미드 출력
     int rows 를 선언해라.
     이 수만큼 다음과 같은 피라미드를 출력하면 된다.
+        *
+        **
+        ***
+        ****
+        *****
+        ******
+        ...
      */
     public static void main(String[] args) {
         Random r = new Random();
