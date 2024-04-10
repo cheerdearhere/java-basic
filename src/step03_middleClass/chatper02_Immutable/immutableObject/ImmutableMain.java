70484e53e203e1e03022e61e0ddea053853f88ee
dreamik <dream-ik89@naver.com>
2024-04-10 16:18:07 +0900

    feat: How to change immutable field

---
+++ src/step03_middleClass/chatper02_Immutable/immutableObject/ImmutableMain.java
@@ -0,0 +1,11 @@
+package step03_middleClass.chatper02_Immutable.immutableObject;
+
+public class ImmutableMain {
+    public static void main(String[] args) {
+        ImmutableObject memberA = new ImmutableObject(10);
+        ImmutableObject memberB = memberA.add(20);
+
+        System.out.println("memberA = " + memberA);
+        System.out.println("memberB = " + memberB);
+    }
+}
---
+++ src/step03_middleClass/chatper02_Immutable/immutableObject/ImmutableObject.java
@@ -0,0 +1,23 @@
+package step03_middleClass.chatper02_Immutable.immutableObject;
+
+public class ImmutableObject {
+    private final int value;
+
+    public ImmutableObject(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public ImmutableObject add(int num){
+        int result = value + num;
+        return new ImmutableObject(result);
+    }
+
+    @Override
+    public String toString() {
+        return "value:" + value;
+    }
+}
