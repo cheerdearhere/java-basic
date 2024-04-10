2833b6e4b0fea94e15229383689b30d6eb379fd1
dreamik <dream-ik89@naver.com>
2024-04-10 16:15:44 +0900

    chore: change field name(a -> field)

--- src/step03_middleClass/chatper02_Immutable/primitiveReference/ReferenceMain.java
+++ src/step03_middleClass/chatper02_Immutable/primitiveReference/ReferenceMain.java
@@ -4,10 +4,10 @@ public class ReferenceMain {
     public static void main(String[] args) {
         ReferenceObject a = new ReferenceObject(10);
         ReferenceObject b = a;
-        b.setA(10+1);
+        b.setField(10+1);
         System.out.println("a, b: " + a + ", "+b);
         ReferenceObject c = a.deepCopy();
-        c.setA(20);
+        c.setField(20);
         System.out.println("a, c: " + a+", "+c);
     }
 }
--- src/step03_middleClass/chatper02_Immutable/primitiveReference/ReferenceObject.java
+++ src/step03_middleClass/chatper02_Immutable/primitiveReference/ReferenceObject.java
@@ -1,23 +1,23 @@
 package step03_middleClass.chatper02_Immutable.primitiveReference;
 
 public class ReferenceObject {
-    private int a;
-    public ReferenceObject(int a) {
-        this.a = a;
+    private int field;
+    public ReferenceObject(int field) {
+        this.field = field;
     }
 
-    public void setA(int a) {
-        this.a = a;
+    public void setField(int field) {
+        this.field = field;
     }
 
-    public int getA() {
-        return a;
+    public int getField() {
+        return field;
     }
     public ReferenceObject deepCopy() {
-        return new ReferenceObject(this.a);
+        return new ReferenceObject(this.field);
     }
     @Override
     public String toString() {
-        return String.valueOf(a);
+        return String.valueOf(field);
     }
 }
