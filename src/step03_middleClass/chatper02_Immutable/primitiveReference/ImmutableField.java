f92732ba5d1e0964982cc10a5c1aeb3ab6fa49bc
dreamik <dream-ik89@naver.com>
2024-04-10 16:16:18 +0900

    feat: using final keyword in reference class

---
+++ src/step03_middleClass/chatper02_Immutable/primitiveReference/ImmutableField.java
@@ -0,0 +1,19 @@
+package step03_middleClass.chatper02_Immutable.primitiveReference;
+
+public class ImmutableField {
+    private final int field;
+    public ImmutableField(int field) {
+        this.field = field;
+    }
+//    public void setField(int field) {
+//        this.field = field;
+//    }
+
+    public int getField() {
+        return field;
+    }
+    @Override
+    public String toString() {
+        return String.valueOf(field);
+    }
+}
---
+++ src/step03_middleClass/chatper02_Immutable/primitiveReference/ImmutableMain.java
@@ -0,0 +1,14 @@
+package step03_middleClass.chatper02_Immutable.primitiveReference;
+
+public class ImmutableMain {
+    public static void main(String[] args) {
+        ImmutableField a = new ImmutableField(10);
+        ImmutableField b = a;
+//        b.setField(10+1); 필드 수정 불가
+        System.out.println("a, b: " + a + ", "+b);
+        ImmutableField c = new ImmutableField(10);
+//        c.setField(20); 수정불가
+        c = new ImmutableField(20);//새로운 객체 생성으로 유도
+        System.out.println("a, c: " + a+", "+c);
+    }
+}
