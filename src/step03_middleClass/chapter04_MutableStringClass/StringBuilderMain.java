67e5ba3c6073505f632a060635a92f52002bff8c
dreamik <dream-ik89@naver.com>
2024-04-16 23:32:11 +0900

    feat: 가변 객체로 만든 문자열 타입

---
+++ src/step03_middleClass/chapter04_MutableStringClass/StringBuilderMain.java
@@ -0,0 +1,46 @@
+package step03_middleClass.chapter04_MutableStringClass;
+
+public class StringBuilderMain {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("A");
+        sb.append("B");
+        sb.append("C");
+        sb.append("D");
+        System.out.println("sb = " + sb);
+
+        sb.insert(3, "Java");
+        System.out.println("sb = " + sb);
+
+        sb.delete(3, 7);
+        System.out.println("sb = " + sb);
+        
+        sb.reverse();
+        System.out.println("sb = " + sb);
+
+        sb.repeat(sb,3);
+        System.out.println("sb = " + sb);
+
+        String immutableCharacterSequence = sb.toString();
+        System.out.println("immutableCharacterSequence = " + immutableCharacterSequence);
+
+//      체크용 시간 측정
+        int count = 100000;
+
+        long startTime = System.currentTimeMillis();
+        String res = "";
+        for(int i=0; i<count; i++) {
+            res += "Hello World!";
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("duration using + operator: "+(endTime-startTime)+" ms");
+
+        startTime = System.currentTimeMillis();
+        String res2="";
+        for(int i=0; i<count; i++) {
+            res2 = new StringBuilder().append("Hello World!").toString();
+        }
+        endTime=System.currentTimeMillis();
+        System.out.println("duration using mutable CharSequence: "+(endTime-startTime)+" ms");
+    }
+}
