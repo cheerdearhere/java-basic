530cbe5849be1a6dd9bdafb246030185b2551f10
dreamik <dream-ik89@naver.com>
2024-04-29 19:08:09 +0900

    feat: Nested class outline, static Nested class

---
+++ src/step03_middleClass/chapter07_InnerClass/NestedOuter.java
@@ -0,0 +1,22 @@
+package step03_middleClass.chapter07_InnerClass;
+
+public class NestedOuter {
+    private static int outClassValue = 3;
+    private int outInstanceValue = 2;
+
+    static class Nested{
+        private int nestedInstanceValue = 1;
+        public void print(){
+            //자신의 멤버
+            //내부 클래스도 본인 클래스 내부이기때문에 private으로 선언해도 사용 가능
+            System.out.println(nestedInstanceValue);
+
+            //외부 클래스의 멤버(outer instance 접근 불가
+//            System.out.printf(outInstanceValue);
+
+            //외부클래스더라도 static은 가능
+            System.out.println(outClassValue);
+        }
+    }
+
+}
