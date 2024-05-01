b0929a72893ba2bb4fb94ff821665139f79cc8ab
dreamik <dream-ik89@naver.com>
2024-05-01 15:20:16 +0900

    feat: 정적 중첩 클래스 - 1

---
+++ src/step03_middleClass/chapter07_InnerClass/NestedOuterMain.java
@@ -0,0 +1,11 @@
+package step03_middleClass.chapter07_InnerClass;
+
+public class NestedOuterMain {
+    public static void main(String[] args) {
+        NestedOuter outer = new NestedOuter();
+        NestedOuter.Nested nested = new NestedOuter.Nested();
+        nested.print();
+
+        System.out.println("Nested Class: "+nested.getClass());
+    }
+}
