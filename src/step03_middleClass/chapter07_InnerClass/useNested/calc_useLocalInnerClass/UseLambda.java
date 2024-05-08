089441735741616cd522e3586fd2484bfbc8ba4f
dreamik <dream-ik89@naver.com>
2024-05-08 17:36:55 +0900

    feat: refactoring with lambda

---
+++ src/step03_middleClass/chapter07_InnerClass/useNested/calc_useLocalInnerClass/UseLambda.java
@@ -0,0 +1,24 @@
+package step03_middleClass.chapter07_InnerClass.useNested.calc_useLocalInnerClass;
+
+import step03_middleClass.chapter07_InnerClass.useNested.calc_useStaticNested.Process;
+
+import java.util.Random;
+
+public class UseLambda {
+    public static void  helloInner(Process process){
+        System.out.println("프로그램 시작");
+        process.run();
+        System.out.println("프로그램 종료");
+    }
+    public static void main(String[] args) {
+        helloInner(()->{
+            int randomValue = new Random().nextInt(6) + 1;
+            System.out.println("주사위: "+randomValue);
+        });
+        helloInner(()->{
+            for(int i = 1; i<=3; i++){
+                System.out.println("i: "+i);
+            }
+        });
+    }
+}
