e3e4a540f5d77ab1055d8ef957e6cddae3448f85
dreamik <dream-ik89@naver.com>
2024-05-16 16:05:06 +0900

    feat: type eraser(compile time and runtime)

---
+++ src/step04_middleClass2/chapter01_Generic/generic05_wildcard/EraserBox.java
@@ -0,0 +1,12 @@
+package step04_middleClass2.chapter01_Generic.generic05_wildcard;
+
+import step04_middleClass2.chapter01_Generic.animal.Animal;
+
+public class EraserBox <T extends Animal>{
+//    public boolean instanceCheck(Object obj){
+//        return obj instanceof T;
+//    }
+//    public void create(){
+//        return new T();
+//    }
+}
