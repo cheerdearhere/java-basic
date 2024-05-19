f0da7e2b42fe2bd6b083963e4bca62c8f3d1d713
dreamik <dream-ik89@naver.com>
2024-05-19 18:58:21 +0900

    feat: preparing for making linkedList

---
+++ src/step04_middleClass2/chapter02_CollectionFramework/linkedList/MyLinkedListV0.java
@@ -0,0 +1,12 @@
+package step04_middleClass2.chapter02_CollectionFramework.linkedList;
+
+public class MyLinkedListV0 {
+    private Node head;
+    private int size = 0;
+    private Node tail;
+    private Object body;
+
+    public MyLinkedListV0(Object body) {
+
+    }
+}
