e6319fcaadd24b8b132ecec9463149abd2f053ed
dreamik <dream-ik89@naver.com>
2024-06-11 17:32:55 +0900

    feat: usecase in java

--- src/step05_designPatterns/singleton/SingletonExample.java
+++ src/step05_designPatterns/singleton/SingletonExample.java
@@ -1,7 +1,8 @@
-package step05_designPatterns.singleton.ex;
+package step05_designPatterns.singleton;
 
-public class RuntimeExample {
+public class SingletonExample {
     public static void main(String[] args) {
+        //java
         Runtime runtime = Runtime.getRuntime();
         System.out.println(runtime.maxMemory());
         System.out.printf(String.valueOf(runtime.freeMemory()));
