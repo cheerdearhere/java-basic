2e7fbe2f64e6820b3769da440c3c13623296a977
dreamik <dream-ik89@naver.com>
2024-11-05 16:29:10 +0900

    fix: rename package name

--- src/step06_advanced1_multiThread_and_concurrency/chapter01_processAndThread/tempClass.java
+++ src/step06_advanced1_multiThread_and_concurrency/chapter01_processAndThread/tempClass.java
@@ -1,4 +1,4 @@
-package step06_advanced1_multiThread_and_concurrency.chapter01_precessAndThread;
+package step06_advanced1_multiThread_and_concurrency.chapter01_processAndThread;
 
 public class tempClass {
     /*파일 구조 세팅을 위한 임시 파일*/
